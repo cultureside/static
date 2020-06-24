b7f7c2639bc446c0f6acfea8101c272ca9a35765
寺澤多実子 <tamiko@terasawatamikonoMacBook-Pro.local>
2020-06-24 16:08:29 +0900

    add files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# static
+# about
+This template is for a small static website.
+
+# usage
+Clone this repo and run "npm install" or "yarn install". After installing the modules, run "npm start" and you can start developing. Enjoy!
---
+++ public/js/main.js
@@ -0,0 +1,27 @@
+"use strict";
+
+$(function () {
+  var ua = navigator.userAgent;
+  console.log(ua);
+
+  if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0 && !(ua.indexOf('Mobile') > 0)) {
+    var viewport = $('head meta[name="viewport"]');
+    viewport.attr('content', 'width=1100');
+  }
+
+  $('a.smooth').click(function () {
+    var href = $(this).attr("href");
+    scroll(href);
+  });
+
+  var scroll = function scroll(href) {
+    var speed = 400;
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $('body,html').animate({
+      scrollTop: position
+    }, speed, 'swing');
+    history.replaceState(null, null, href);
+    return false;
+  };
+});
